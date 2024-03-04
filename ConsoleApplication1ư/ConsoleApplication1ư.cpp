a74228f6651b64b3833ca9eb054f5e92c33bac8e
ADMIN <ADMIN@LAB1-MAY31>
2024-03-04 09:08:49 +0700

    Add project files.

---
+++ ConsoleApplication1ư/ConsoleApplication1ư.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int sodau, socuoi;
+	cout << "Nhap so dau:";
+	cin >> sodau;
+	cout << "Nhap so cuoi:";
+	cin >> socuoi;
+	cout << "VONG LAP WHILE\n";
+}
+	while (sodau <= socuoi) {
+		cout << sodau << "  ";
+		sodau++;
+		cout << "\nVONG LAP DO WHILE\n";
+		{ 
+			cout << sodau << " ";
+		} while (sodau <= socuoi);
+		return 0;
+
+}
